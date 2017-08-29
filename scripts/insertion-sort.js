731a1692d8de1c43e5cfd55f12947e8098e9b45a
Nick Teets <nickteets@gmail.com>
2017-08-28 18:57:19 -0600

    Gitastrophe

---
+++ scripts/insertion-sort.js
@@ -0,0 +1,16 @@
+const insertionSort = (array) => {
+
+  for (let i = 0; i < array.length; i++) {
+    for (var j = i-1; j >= 0; j--) {
+      if (array[j+1] < array[j]) {
+        [ array[j], array[j+1] ] = [array[j+1], array[j]]
+      }
+    }
+  }
+
+  return array
+};
+
+test
+
+export default insertionSort
