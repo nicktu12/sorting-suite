04ee132812e06063845e45270a4297c69e11a388
Nick Teets <nickteets@gmail.com>
2017-09-09 15:37:11 -0600

    Remove vars from code

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -1,7 +1,7 @@
 const bubbleSort = (array) => {
 
   for (let i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length; j++) {
+    for (let j = 0; j < array.length; j++) {
       if (array[j] > array[j+1]) {
         [ array[j], array[j+1] ] = [array[j+1], array[j]]
       }
